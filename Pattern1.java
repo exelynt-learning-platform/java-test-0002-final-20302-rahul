002f8e2d9518dfc6d6297ba3578ae8392bdb569c
RahulMhetre <rahulmhetre82@gmail.com>
2026-03-14 15:31:17 +0530

    Refactor Pattern1.java for consistent formatting

--- Pattern1.java
+++ Pattern1.java
@@ -7,17 +7,14 @@ public class Pattern1 {
 
 	        for (int i = 1; i <= n; i++) {
 
-	            // print spaces
 	            for (int j = 1; j <= n - i; j++) {
 	                System.out.print("  ");
 	            }
 
-	            // increasing numbers
 	            for (int j = 1; j <= i; j++) {
 	                System.out.print(j + " ");
 	            }
 
-	            // decreasing numbers
 	            for (int j = i - 1; j >= 1; j--) {
 	                System.out.print(j + " ");
 	            }
